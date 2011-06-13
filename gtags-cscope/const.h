32bbc81d645e534492e28fa2c5badacc7688d5ed
lyrical_logical <lyrical.logical@gmail.com>
2011-06-13 22:05:40 +0900

    global 5.9.6

---
+++ gtags-cscope/const.h
@@ -0,0 +1,42 @@
+/* This file is generated automatically by convert.pl from gtags-cscope/manual.in. */
+const char *progname = "gtags-cscope";
+const char *usage_const = "Usage: gtags-cscope [-bCdehLlVv][-F symfile ][-0123456789 pattern][-p n]\n";
+const char *help_const = "Options:\n\
+-h\n\
+       View the long usage help display.\n\
+-V\n\
+       Print on the first line of screen the version number of gtags-cscope.\n\
+--help\n\
+       Same as -h\n\
+--version\n\
+       Same as -V\n\
+-b\n\
+       Build the cross-reference only.\n\
+-C\n\
+       Ignore letter case when searching.\n\
+-d\n\
+       Do not update the cross-reference.\n\
+-e\n\
+       Suppress the <Ctrl>-e command prompt between files.\n\
+-F symfile\n\
+       Read symbol reference lines from symfile. \n\
+       (A symbol reference file is created by > and >>,\n\
+       and can also be read using the < command,\n\
+       described under ``Issuing Subsequent Requests'', below.)\n\
+-L\n\
+       Do a single search with line-oriented output when used with the\n\
+       -num pattern option.\n\
+-l\n\
+       Line-oriented interface (see ``Line-Oriented Interface'' below).\n\
+-[0-9] pattern\n\
+       Go to input field num (counting from 0) and find \n\
+       pattern.\n\
+-p n\n\
+       Display the last n file path components instead of\n\
+       the default (1). Use 0 not to display the file name at all.\n\
+-v\n\
+       Be more verbose in line-oriented mode.  Output progress updates during\n\
+       database building and searches.\n\
+See also:\n\
+       GNU GLOBAL web site: http://www.gnu.org/software/global/\n\
+";
